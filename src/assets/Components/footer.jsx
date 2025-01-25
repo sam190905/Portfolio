98bded110479fa9ec95e1eab5794a3c55b2a6472
Samarth Purant <samarthpurant018@gmail.com>
2025-01-26 00:47:44 +0530

    solved footer logo issue

--- src/assets/Components/footer.jsx
+++ src/assets/Components/footer.jsx
@@ -4,17 +4,17 @@ function Footer(){
     return <div className="footerbox">
         <div className="links">
         <a href="https://github.com/sam190905" target="_blank" rel="noopener noreferrer">
-  <img className="footertile" src="../../../public/linklogos/github.png" alt="GitHub Logo" />
+  <img className="footertile" src="/linklogos/github.png" alt="GitHub Logo" />
 </a>
 <a href="https://instagram.com/@sam190905" target="_blank" rel="noopener noreferrer">
-  <img className="footertile" src="../../../public/linklogos/instagram.png" alt="Instagram Logo" />
+  <img className="footertile" src="/linklogos/instagram.png" alt="Instagram Logo" />
 </a>
 <a href="https://www.linkedin.com/in/samarth-purant-0560a2301/" target="_blank" rel="noopener noreferrer">
-  <img className="footertile" src="../../../public/linklogos/linkedin-logo.png" alt="LinkedIn Logo" />
+  <img className="footertile" src="/linklogos/linkedin-logo.png" alt="LinkedIn Logo" />
 </a>
 
         <a href="https://www.pexels.com/@samarth-purant-1074338883/" target="_blank" rel="noopener noreferrer">
-  <img className="footertile" src="../../../public/linklogos/free.png" alt="Free Logo" />
+  <img className="footertile" src="/linklogos/free.png" alt="Free Logo" />
 </a>
 
         </div>
